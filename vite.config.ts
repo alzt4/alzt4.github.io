17bf3821948fdc8ae72a83535ead8c81a3c952cc
alzt4 <adrianlimzhengting@trentu.ca>
2025-06-16 17:14:37 -0400

    changed base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.exr']
+  assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.exr'],
+  base: "https://altz4.github.io/"
 })
